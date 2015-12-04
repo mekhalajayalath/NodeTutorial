b327b2535ae473a0f187b0c48b1ffd1a43efc175
mekhalajs <mekhalajs@gmail.com>
2015-12-04 14:34:02 +0530

    Modules

--- app.js
+++ app.js
@@ -2,27 +2,6 @@
  * Created by mekhala on 12/3/15.
  */
 
-function user() {
-    this.name = "";
-    this.life = 100;
-    this.giveLife = function(targetedPlayer){
-        targetedPlayer.life += 1;
-        console.log(this.name + " gave a life to " + targetedPlayer.name);
-        console.log(targetedPlayer.name + " has " + targetedPlayer.life + " lives now. ")
-    }
-}
+var movies = require("./movies");
+movies.avatar();
 
-var mekhala = new user();
-var samiyuru = new user();
-mekhala.name = "Mekh";
-samiyuru.name = "Sam";
-
-samiyuru.giveLife(mekhala);
-
-user.prototype.punch = function punch(targetedPlayer){
-    targetedPlayer.life -= 3;
-    console.log(this.name + " punched " + targetedPlayer.name);
-    console.log(targetedPlayer.name + " has " + targetedPlayer.life + " lives now. ")
-}
-
-mekhala.punch(samiyuru);
---
+++ movies.js
@@ -0,0 +1,14 @@
+/**
+ * Created by mekhala on 12/4/15.
+ */
+
+
+function printAvatar(){
+    console.log("Avatar");
+}
+
+function printChappie(){
+    console.log("Chappie");
+}
+
+module.exports.avatar = printAvatar;
